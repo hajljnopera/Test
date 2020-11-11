d4f0074434bb6f95bd233e765afa92fed96477d2
hajljnopera <39905585+hajljnopera@users.noreply.github.com>
2020-11-11 08:07:44 +0700

    Add files via upload
    Test js

---
+++ Array.js
@@ -0,0 +1,13 @@
+
+
+Array.prototype.random = function () {
+  return this[Math.floor((Math.random()*this.length))];
+}
+
+
+// https://stackoverflow.com/a/12623365
+Array.prototype.contains = function(element){
+    return this.indexOf(element) > -1;
+};
+
+
