8e86b3c58b7f777081bb6145ca1d4933564a90a9
hajljnopera <39905585+hajljnopera@users.noreply.github.com>
2020-12-19 12:23:18 +0700

    Add files via upload

---
+++ test_scripting.js
@@ -0,0 +1,9 @@
+
+
+_testWallpaperChangerPlugin();
+
+
+function _testWallpaperChangerPlugin() {
+	var folder = 'D:\\Photo\\Wallpaper\\Offical\\Animal';
+	WallpaperChangerPlugin.Change(folder, WallpaperChangeMode.Random, WallpaperStyle.Fill);
+}
