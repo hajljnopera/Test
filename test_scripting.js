7b047879847b00221c8f61e865fef2650ab4b503
hajljnopera <39905585+hajljnopera@users.noreply.github.com>
2020-12-19 12:34:44 +0700

    idm send link

--- test_scripting.js
+++ test_scripting.js
@@ -2,8 +2,18 @@
 
 _testWallpaperChangerPlugin();
 
+var url = 'https://i1.sndcdn.com/artworks-000127760511-l5ur7k-t500x500.jpg';
+sendLinkIDM(url);
+
 
 function _testWallpaperChangerPlugin() {
 	var folder = 'D:\\Photo\\Wallpaper\\Offical\\Animal';
 	WallpaperChangerPlugin.Change(folder, WallpaperChangeMode.Random, WallpaperStyle.Fill);
 }
+
+function sendLinkIDM(url) {
+	IDM.SendLink2(url, '', '', '', '', '', '', '', 0);
+}
+
+
+
