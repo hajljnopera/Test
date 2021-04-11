872b621713e3a73c1f65ae6f1d8f02ad13d564ae
hajljnopera <39905585+hajljnopera@users.noreply.github.com>
2021-04-11 08:40:51 +0700

    Update Array.js

--- Array.js
+++ Array.js
@@ -11,4 +11,43 @@ Array.prototype.contains = function(element){
 };
 
 
-// Comment goes here !!!
+String.prototype.append = function(w) {
+  if (this.endsWith(w)) return this;
+  return this + w;
+}
+
+String.prototype.prepend = function(w) {
+  if (this.startsWith(w)) return this;
+  return w + this;
+}
+
+String.prototype.rtrim = function(s) {
+    if (s == undefined) s = '\\s';
+    return this.replace(new RegExp("[" + s + "]*$"), '');
+}
+
+String.prototype.ltrim = function(s) {
+    if (s == undefined) s = '\\s';
+    return this.replace(new RegExp("^[" + s + "]*"), '');
+}
+
+// https://stackoverflow.com/a/4673436
+if (!String.format) {
+  String.format = function(format) {
+    var args = Array.prototype.slice.call(arguments, 1);
+    return format.replace(/{(\d+)}/g, function(match, number) { 
+      return typeof args[number] != 'undefined'
+        ? args[number] 
+        : match
+      ;
+    });
+  };
+}
+
+
+
+
+
+var upCase = function(s) {
+	return s.toUpperCase();
+}
